93892f8ba086268bf402f1208483487b73d14c37
xhanyifeng <xhanyifeng@gmail.com>
2018-05-22 11:34:22 +0800

    add 80 web

---
+++ web.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+RESPONSE="HTTP/1.1 200 OK\r\nConnection: keep-alive\r\n\r\n${2:-"OK"}\r\n"
+while { echo -en "$RESPONSE"; } | nc -l "${1:-80}"; do
+  echo "================================================"
+done
