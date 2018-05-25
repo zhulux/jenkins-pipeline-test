75386e6fa3217a50e7ba76d154a84a6ca4a4de90
xhanyifeng <xhanyifeng@gmail.com>
2018-05-25 23:40:39 +0800

    add dao-od

---
+++ dao-od-gem.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# build and push gem to private hosting server
+# NOTE: running in a standalone ruby container in CI process!
+
+echo ":push-key: $PUSH_KEY" > ~/.gem/credentials
+chmod 600 ~/.gem/credentials
+cd ./optimus_data
+
+# just triggered by tags: od-66.xx.xx
+version=$(echo $DAO_COMMIT_TAG | cut -d - -f 2)
+echo ==parsed od gem version: $version
+export OD_GEM_BUILD_VERSION=$version 
+gem build optimus_data.gemspec
+
+gem push optimus_data-*.gem --host "$GEM_SERVER" --key push-key -V
+echo Congrats $version gem pushed!
