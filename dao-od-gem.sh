f7265e95e5265bcac4b77b64b77317e33bc38129
xhanyifeng <xhanyifeng@gmail.com>
2018-05-29 14:05:04 +0800

    test od

--- dao-od-gem.sh
+++ dao-od-gem.sh
@@ -2,7 +2,8 @@
 # build and push gem to private hosting server
 # NOTE: running in a standalone ruby container in CI process!
 
-sudo mkdir ~/.gem
+cd ~
+mkdir .gem
 echo ":push-key: $PUSH_KEY" > ~/.gem/credentials
 chmod 600 ~/.gem/credentials
 cd ./optimus_data
